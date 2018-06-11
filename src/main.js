b789d1dd45b12edf004f246eb04064169a1a1e61
Aviran Katz <aviran.katz@gmail.com>
2018-06-11 20:06:29 +0300

    PWA migration

--- src/main.js
+++ src/main.js
@@ -8,20 +8,29 @@ import requester from './mixins/requester'
 import VueRouter from 'vue-router';
 import Punch from './components/Punch.vue';
 import Summary from './components/Summary.vue';
-
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale });
 Vue.use(VueResource);
+
+//#region vue router
 Vue.use(VueRouter);
 const routes = [
   { path: '/', component: Punch },
   { path: '/summary', component: Summary },
-]
+];
 const router = new VueRouter({
   routes // short for `routes: routes`
-})
+});
+//#endregion
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js')
+    .then(() => console.log('service worker registered'));
+}
+
+
 new Vue({
   render: h => h(App),
   mixins: [requester],
   router: router
-}).$mount('#app')
+}).$mount('#app');
